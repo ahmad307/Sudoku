20c0090f01ed694238d51f6cfbbb38c7e7e05020
Ahmad <ahmad.hussain.307@gmail.com>
2018-04-07 16:23:16 +0200

    Added GetBoard Proc,Renamed data files

--- Assembly_Project/main.asm
+++ Assembly_Project/main.asm
@@ -1,27 +1,32 @@
-INCLUDE Irvine32.inc
 
-;islam : getValue,editCell
-;ahmad : getBoard,checkIndex,checkAnswer
-;Hadil : readArray,takeInput
-;Raamyy: checkAvailble,getDifficulty,printArr
+;islam : GetValue,EditCell
+;ahmad : GetBoard,CheckIndex,CheckAnswer
+;Hadil : ReadArray,TakeInput
+;Raamyy: CheckAvailble,GetDifficulty,PrintArr
+
+INCLUDE Irvine32.inc
 
 .data
 
-board Byte 81 DUP(?) ;sudoko board
-xCor Byte ? ;x coordinate
-yCor Byte ? ;y coordinate
-num Byte ?  ;user number to update
-difficulty Byte ? ;1 Easy, 2 Medium, 3 Hard
+board Byte 81 DUP(?)    ;sudoko board
+xCor Byte ?		;x coordinate
+yCor Byte ?     ;y coordinate
+num Byte ?    ;user number to update
+difficulty Byte ?	;1 Easy, 2 Medium, 3 Hard
 fileName Byte 10 Dup(?), 0
 wrongCounter Byte ?
 correctCounter Byte ?
 remainingCounter Byte ?
-DifficultyMessage Byte "Please Enter the difficulty",0
+difficultyMessage Byte "Please Enter the difficulty",0
+fileName Byte "sudoku_boards/diff_?_?.txt",0
+solvedFileName Byte "sudoku_boards/diff_?_?_solved.txt",0
+
+
 .code
 
-;Read the array from the file
-;param Edx offset of the array
-;param Ebx offset of string file name
+;Reads the array from the file
+;param: Edx offset of the array
+;param: Ebx offset of string file name
 ReadArray PROC
 
 	ret
@@ -74,12 +79,36 @@ GetValue PROC
 	ret
 GetValue ENDP
 
-;Return sudoko Board
-;param Difficulty (Easy, Medium, Hard)
+
+
+;Param:	 Difficulty (global var)
+;Returns: Desired board in board var
 GetBoard PROC
+	;Generating random value from ax and cx
+	xor ax,cx
+
+	;Getting the value modulu 3
+	mov cx,ax
+	mov dx,0
+	mov bx,3
+	div bx		;BX carries a random value less than 3
+
+	;Customizing fileName string with difficulty and random choice
+	mov al,bx
+	mov fileName[21],al
+
+	mov al,difficulty
+	mov fileName[19],al
+
+	;Calling ReadArr with required params to populate board var
+	mov edx,offset board
+	mov ebx,offset fileName
+	call ReadArr
 
 	ret
 GetBoard ENDP
+
+
 
 ;param Edx offset of array
 PrintArray PROC
@@ -121,6 +150,7 @@ GetDifficulty PROC
 	call WriteString
 	call crlf
 
+	;Better use ReadChar for GetBoard Proc;
 	call ReadDec
 	mov difficulty,al ;take the byte from eax
 	
