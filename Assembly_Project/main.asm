72ce709bdfb2db29191b04f0ec9cf48ec61a1b90
Hadil elrody <elrody52@gmail.com>
2018-04-07 18:21:05 -0700

    Added ReadArray Proc

--- Assembly_Project/main.asm
+++ Assembly_Project/main.asm
@@ -1,5 +1,6 @@
 INCLUDE Irvine32.inc
-
+INCLUDE macros.inc
+BUFFER_SIZE=5000
 ;islam : getValue,editCell
 ;ahmad : getBoard,checkIndex,checkAnswer
 ;Hadil : readArray,takeInput
@@ -17,12 +18,98 @@ wrongCounter Byte ?
 correctCounter Byte ?
 remainingCounter Byte ?
 DifficultyMessage Byte "Please Enter the difficulty",0
+buffer BYTE BUFFER_SIZE DUP(?)
+fileHandle HANDLE ?
+
 .code
 
 ;Read the array from the file
 ;param Edx offset of the array
 ;param Ebx offset of string file name
 ReadArray PROC
+; Let user input a filename.
+;27 is constant size for filename
+mov ecx,27
+; Open the file for input.
+
+mov edx,ebx
+call OpenInputFile
+mov fileHandle, eax
+
+; Check for errors.
+cmp eax, INVALID_HANDLE_VALUE; error opening file ?
+jne file_ok; no: skip
+mWrite <"Cannot open file", 0dh, 0ah>
+jmp quit; and quit
+
+file_ok :
+; Read the file into a buffer.
+mov edx, OFFSET buffer
+mov ecx, BUFFER_SIZE
+call ReadFromFile
+jnc check_buffer_size; error reading ?
+mWrite "Error reading file. "; yes: show error message
+call WriteWindowsMsg
+jmp close_file
+
+check_buffer_size :
+cmp eax, BUFFER_SIZE; buffer large enough ?
+jb buf_size_ok; yes
+mWrite <"Error: Buffer too small for the file", 0dh, 0ah>
+jmp quit; and quit
+
+buf_size_ok :
+mov buffer[eax], 0; insert null terminator
+;mWrite "File size: "
+;call WriteDec; display file size
+;call Crlf
+
+mov edx, OFFSET buffer; display the buffer
+mov esi, edx
+
+mov ecx, 97
+mov edx, offset board
+
+l :
+  mov al, [esi]
+  inc esi
+  cmp al, 13
+  je line
+  cmp al, 10
+  je line
+  mov[edx], al
+  inc edx
+
+  line :
+loop l
+
+
+   mov esi, offset board
+   mov ecx, 81
+   mov eax, 0
+l1:
+
+  mov al, [esi]
+  add esi, 1
+
+loop l1
+
+mov esi, offset  board
+mov ecx, 81
+l2:
+
+   sub byte ptr[esi],48
+   inc esi 
+
+loop l2
+
+mov edx,offset board
+
+close_file :
+mov eax, fileHandle
+call CloseFile
+
+quit :
 
 	ret
 ReadArray ENDP
@@ -116,17 +203,17 @@ again:
 mWrite "Enter the x coordinate :  " 
 call WriteWindowsMsg
 call ReadDec
-mov xCor,eax
+mov xCor,al
 
 mWrite "Enter the y coordinate :  " 
 call WriteWindowsMsg
 call ReadDec
-mov yCor,eax
+mov yCor,al
 
 mWrite "Enter the number :  " 
 call WriteWindowsMsg
 call ReadDec
-mov num,eax
+mov num,al
 
 call CheckIndex
 cmp eax ,1
